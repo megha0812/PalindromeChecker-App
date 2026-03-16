7f46b8170c0482fd40e9a89f10248675035c72f0
megha0812 <meghakadiyala12@gmail.com>
2026-03-16 09:38:06 +0530

    megha kadiyala Added UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,24 +1,57 @@
-// version 3.0
+// version 13.0
 //author megha kadiyala
 //usecase 1: Welcome Page
 //usecase 2: Check Hardcoded String for Palindrome
 //usecase 3: Palindrome Check using String Reverse
-import java.util.Scanner;
-public class UseCase3PalindromeChecker {
+//usecase 13: Performance Comparison of Algorithms
+import java.util.*;
+public class PalindromePerformance {
+    public static boolean simpleCheck(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static boolean stackCheck(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("=== UC3: Palindrome Check (String Reverse Method) ===");
-        System.out.print("Enter a word or sentence: ");
-        String original = scanner.nextLine();
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed += original.charAt(i);   // String concatenation
-        }
-        if (original.equals(reversed)) {
-            System.out.println("Result: \"" + original + "\" is a PALINDROME.");
-        } else {
-            System.out.println("Result: \"" + original + "\" is NOT a palindrome.");
-        }
-        scanner.close();
+
+        String input = "madam";
+
+        long start1 = System.nanoTime();
+        simpleCheck(input);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        stackCheck(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Simple Method Time: " + (end1 - start1) + " ns");
+        System.out.println("Stack Method Time: " + (end2 - start2) + " ns");
     }
 }
